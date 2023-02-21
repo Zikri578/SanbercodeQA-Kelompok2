0440c6700ef532eff4980d2c093307ca861d3902
icefrog111 <icefrog111@gmail.com>
2023-02-21 17:37:33 +0700

    PIM Add Employee Sucess

--- TestPIM_Employee_DIego.py
+++ TestPIM_Employee_DIego.py
@@ -9,64 +9,76 @@ class TestLogin(unittest.TestCase):
     def setUp(self): 
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
         
-    def test__nama(self): 
-        # steps
-        browser = self.browser #buka web browser
-        browser.get("http://opensource-demo.orangehrmlive.com/")
+    def test_nama(self): 
+        browser = self.browser
+        browser.get("https://opensource-demo.orangehrmlive.com/")
+        self.browser.maximize_window()
+        time.sleep(5)
         browser.find_element(By.NAME, 'username').send_keys('Admin')
-        time.sleep(1)
+        time.sleep(3)
         browser.find_element(By.NAME, 'password').send_keys('admin123')
-        time.sleep(1)
-        browser.find_element(By.CLASS_NAME, 'oxd-button oxd-button--medium oxd-button--main orangehrm-login-button').click()
-        time.sleep(1)
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//span[normalize-space()='PIM']").click()
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//button[normalize-space()='Add']").click()
+        time.sleep(3)
         browser.find_element(By.NAME, 'firstName').send_keys('Diego')
-        time.sleep(1)
+        time.sleep(3)
         browser.find_element(By.NAME, 'middleName').send_keys('Yanda')
-        time.sleep(1)
+        time.sleep(3)
         browser.find_element(By.NAME, 'lastName').send_keys('Setiawan')
-        time.sleep(1)
-        browser.find_element(By.CLASS_NAME,"oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space").click()
-        time.sleep(1)
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//button[normalize-space()='Save']").click()
+        time.sleep(3)
         
-    def test__ID(self): 
-        # steps
-        browser = self.browser #buka web browser
-        browser.get("http://opensource-demo.orangehrmlive.com/")
+    def test_cancel_employee(self): 
+        browser = self.browser
+        browser.get("https://opensource-demo.orangehrmlive.com/")
+        self.browser.maximize_window()
+        time.sleep(5)
         browser.find_element(By.NAME, 'username').send_keys('Admin')
-        time.sleep(1)
+        time.sleep(3)
         browser.find_element(By.NAME, 'password').send_keys('admin123')
-        time.sleep(1)
-        browser.find_element(By.CLASS_NAME, 'oxd-button oxd-button--medium oxd-button--main orangehrm-login-button').click()
-        time.sleep(1)   
-        browser.find_element(By.CLASS_NAME, 'oxd-input oxd-input--active').send_keys('111')
-        time.sleep(1)
-        browser.find_element(By.CLASS_NAME,"oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space").click()
-        time.sleep(1)
-        
-    def save__employee(self): 
-        # steps
-                # steps
-        browser = self.browser #buka web browser
-        browser.get("http://opensource-demo.orangehrmlive.com/")
-        browser.find_element(By.NAME, 'username').send_keys('Admin')
-        time.sleep(1)
-        browser.find_element(By.NAME, 'password').send_keys('admin123')
-        time.sleep(1)
-        browser.find_element(By.CLASS_NAME, 'oxd-button oxd-button--medium oxd-button--main orangehrm-login-button').click()
-        time.sleep(1)
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//span[normalize-space()='PIM']").click()
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//button[normalize-space()='Add']").click()
+        time.sleep(3)
         browser.find_element(By.NAME, 'firstName').send_keys('Diego')
-        time.sleep(1)
+        time.sleep(3)
         browser.find_element(By.NAME, 'middleName').send_keys('Yanda')
-        time.sleep(1)
+        time.sleep(3)
         browser.find_element(By.NAME, 'lastName').send_keys('Setiawan')
-        time.sleep(1)
-        browser.find_element(By.CLASS_NAME, 'oxd-input oxd-input--active').send_keys('111')
-        time.sleep(1)
-        browser.find_element(By.CLASS_NAME,"oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space").click()
-        time.sleep(1)
+        time.sleep(3)
+        setup.find_element(By.XPATH, "//button[normalize-space()='Cancel']").click()
+        time.sleep(3)
         
-    def tearDown(self): 
-        self.browser.close() 
+    def test_save_employee(self): 
+        browser = self.browser
+        browser.get("https://opensource-demo.orangehrmlive.com/")
+        self.browser.maximize_window()
+        time.sleep(5)
+        browser.find_element(By.NAME, 'username').send_keys('Admin')
+        time.sleep(3)
+        browser.find_element(By.NAME, 'password').send_keys('admin123')
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//span[normalize-space()='PIM']").click()
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//button[normalize-space()='Add']").click()
+        time.sleep(3)
+        browser.find_element(By.NAME, 'firstName').send_keys('Diego')
+        time.sleep(3)
+        browser.find_element(By.NAME, 'middleName').send_keys('Yanda')
+        time.sleep(3)
+        browser.find_element(By.NAME, 'lastName').send_keys('Setiawan')
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//button[normalize-space()='Save']").click()
+        time.sleep(3)
 
-if __name__ == "__main__": 
-    unittest.main()
+unittest.main()
