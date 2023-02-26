80b872976c0bc9cd480f5d7bf15622abc36d4884
Musta <shodiq5mustofa@gmail.com>
2023-02-26 16:07:00 +0700

    Update Test Case
    Update Test Case

--- Test_Suite_Admin_Organization_Locations/Test_Edit_Location/Edit_Location.py
+++ Test_Suite_Admin_Organization_Locations/Test_Edit_Location/Edit_Location.py
@@ -2,6 +2,7 @@ import unittest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 #from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 from page import elem
@@ -30,16 +31,100 @@ class TestEdit(unittest.TestCase):
         time.sleep(1)
         browser.find_element(By.XPATH,"//*[@id='app']/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[7]/div/button[2]").click()
         time.sleep(3)
-        browser.find_element(By.XPATH,"//input[@class= 'oxd-input oxd-input--active']").clear()
-        browser.find_element(By.XPATH,"//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div/div/div[2]/input").send_keys("Software In")
-        time.sleep(1)
-        browser.find_element(By.XPATH,"//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[1]/div/div[2]/input").clear()
-        time.sleep(1)
-        browser.find_element(By.XPATH,"//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[1]/div/div[2]/input").send_keys("Jakarta")
+        driver = browser.find_element(By.XPATH,elem.edit_city)
+        driver.send_keys(Keys.CONTROL + "a")
+        driver.send_keys(Keys.BACKSPACE)
+        driver.send_keys("Jakarta")
         time.sleep(1)
         browser.find_element(By.XPATH,elem.edit_slc_country).click()
         browser.find_element(By.XPATH,elem.edit_search_country).click()
-        time.sleep(1)       
+        time.sleep(1)   
+        browser.find_element(By.XPATH,elem.edit_btn_save).click()
+        time.sleep(1)    
+
+        #Assert
+        response_data = browser.find_element(By.XPATH,elem.edit_assert).text
+        self.assertIn(response_data,"OrangeHRM OS 5.3")
+
+    def test_b_edit_failed(self):
+        browser = self.browser
+
+        browser.get(elem.base_url)
+        self.browser.maximize_window()
+        time.sleep(3)
+        browser.find_element(By.NAME, "username").send_keys("Admin") #Right Username
+        browser.find_element(By.NAME, "password").send_keys("admin123") #Right Password
+        browser.find_element(By.CLASS_NAME, "oxd-button").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH,elem.admin).click() #Open Menu Admin
+        time.sleep(1)
+        browser.find_element(By.XPATH,elem.org).click() 
+        time.sleep(1)
+        browser.find_element(By.XPATH,elem.loc).click() #Open Menu Location
+        time.sleep(1)
+        browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        time.sleep(1)
+        browser.find_element(By.XPATH,elem.edit_icon_btn).click()
+        time.sleep(3)
+        driver = browser.find_element(By.XPATH,elem.edit_city)
+        driver.send_keys(Keys.CONTROL + "a")
+        driver.send_keys(Keys.BACKSPACE)
+        driver.send_keys("Jaaaaakkkkkkaaaarrrrrrrrttttttttttttttttttaaaaaaaaaaaaaa")
+        time.sleep(1)
+        browser.find_element(By.XPATH,elem.edit_slc_country).click()
+        browser.find_element(By.XPATH,elem.edit_search_country).click()
+        time.sleep(1)   
+        driver = browser.find_element(By.XPATH,elem.edit_phone)
+        driver.send_keys(Keys.CONTROL + "a")
+        driver.send_keys(Keys.BACKSPACE)
+        driver.send_keys("62 787887676O")
+        time.sleep(1)
+        browser.find_element(By.XPATH,elem.edit_btn_save).click()
+        time.sleep(1)    
+
+        #Assert
+        response_data = browser.find_element(By.XPATH,elem.edit_fail_assert).text
+        self.assertIn(response_data,"Should not exceed 50 characters")
+
+    def test_c_edit_cancel(self):
+        browser = self.browser
+
+        browser.get(elem.base_url)
+        self.browser.maximize_window()
+        time.sleep(3)
+        browser.find_element(By.NAME, "username").send_keys("Admin") #Right Username
+        browser.find_element(By.NAME, "password").send_keys("admin123") #Right Password
+        browser.find_element(By.CLASS_NAME, "oxd-button").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH,elem.admin).click() #Open Menu Admin
+        time.sleep(1)
+        browser.find_element(By.XPATH,elem.org).click() 
+        time.sleep(1)
+        browser.find_element(By.XPATH,elem.loc).click() #Open Menu Location
+        time.sleep(1)
+        browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        time.sleep(1)
+        browser.find_element(By.XPATH,elem.edit_icon_btn).click()
+        time.sleep(3)
+        driver = browser.find_element(By.XPATH,elem.edit_city)
+        driver.send_keys(Keys.CONTROL + "a")
+        driver.send_keys(Keys.BACKSPACE)
+        driver.send_keys("Jaaaaakkkkkkaaaarrrrrrrrttttttttttttttttttaaaaaaaaaaaaaa")
+        time.sleep(1)
+        browser.find_element(By.XPATH,elem.edit_slc_country).click()
+        browser.find_element(By.XPATH,elem.edit_search_country).click()
+        time.sleep(1)   
+        driver = browser.find_element(By.XPATH,elem.edit_phone)
+        driver.send_keys(Keys.CONTROL + "a")
+        driver.send_keys(Keys.BACKSPACE)
+        driver.send_keys("62 787887676O")
+        time.sleep(1)
+        browser.find_element(By.XPATH,elem.edit_btn_cancel).click()
+        time.sleep(1)    
+
+        #Assert
+        response_data = browser.find_element(By.XPATH,elem.edit_cancel_assert).text
+        self.assertIn(response_data,"Locations")
 
 if __name__ == "__main__": 
     unittest.main()
--- Test_Suite_Admin_Organization_Locations/Test_Edit_Location/page.py
+++ Test_Suite_Admin_Organization_Locations/Test_Edit_Location/page.py
@@ -9,5 +9,15 @@ class elem():
     loc = "//li[normalize-space()='Locations']"
 
     #Elemen
+    edit_icon_btn = "//*[@id='app']/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[7]/div/button[2]"
+    edit_city = "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[1]/div/div[2]/input"
     edit_slc_country = "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[4]/div/div[2]/div/div/div[1]"
-    edit_search_country = "//div[@class='oxd-select-dropdown --positon-bottom']//span[normalize-space()='Indonesia']" #Select Country
+    edit_search_country = "//div[@class='oxd-select-dropdown --positon-bottom']//span[normalize-space()='Gabon']" #Select Country
+    edit_phone = "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[5]/div/div[2]/input"
+    edit_btn_save = "//Button[normalize-space() = 'Save']"
+    edit_btn_cancel = "//Button[normalize-space() = 'Cancel']"
+
+    #Assert
+    edit_assert = "//p[normalize-space()='OrangeHRM OS 5.3']"
+    edit_fail_assert = "//span[normalize-space()='Should not exceed 50 characters']"
+    edit_cancel_assert = "//div[normalize-space()='Locations']"
