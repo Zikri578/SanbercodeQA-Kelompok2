3dd9dbca5972b591ffe6d91104b97e379c690412
Mochammad Itmamul Wafa <itmamulwafa11@yahoo.com>
2023-02-26 23:25:36 +0700

    update cancel delete

--- Test_Suite_Admin_Job_JobTilte/Add_job_title.py
+++ Test_Suite_Admin_Job_JobTilte/Add_job_title.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from PIL import Image
 
 
 
@@ -11,7 +12,63 @@ class TestAddJobTitle(unittest.TestCase):
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
 
 
+    def test_cancel_delete_data(self):
+        # steps Login
+        browser = self.browser
+        browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        time.sleep(1)
+        browser.find_element(By.NAME, "username").send_keys("Admin")
+        time.sleep(1)
+        browser.find_element(By.NAME, "password").send_keys("admin123")
+        time.sleep(1)
+        browser.find_element(By.CLASS_NAME, "oxd-button").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH, "//span[normalize-space()='Admin']").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH, "//span[normalize-space()='Job']").click()
+        time.sleep(2)
+        browser.find_element(By.LINK_TEXT, 'Job Titles').click()
+        time.sleep(2)
+
+        # validasi
+        current_url = browser.current_url
+        self.assertEqual(current_url, "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList")
+        delete_2 = browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div[11]/div/div/div[1]/div[2]/div/div/button[1]").click()
+        self.assertIn('Database Administrator', delete_2)
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[3]/div/div/div/div[3]/button[1]").click()
+        time.sleep(2)
+        
+
     def test_delete_data(self):
+        # steps Login
+        browser = self.browser
+        browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        time.sleep(1)
+        browser.find_element(By.NAME, "username").send_keys("Admin")
+        time.sleep(1)
+        browser.find_element(By.NAME, "password").send_keys("admin123")
+        time.sleep(1)
+        browser.find_element(By.CLASS_NAME, "oxd-button").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH, "//span[normalize-space()='Admin']").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH, "//span[normalize-space()='Job']").click()
+        time.sleep(2)
+        browser.find_element(By.LINK_TEXT, 'Job Titles').click()
+        time.sleep(2)
+
+        # validasi
+        current_url = browser.current_url
+        self.assertEqual(current_url, "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList")
+
+        delete_1 = browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div[7]/div/div/div[1]/div[2]/div/div/button[1]/i").text.click()
+        self.assertIn('Database Administrator', delete_1)
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[3]/div/div/div/div[3]/button[2]").click()
+        time.sleep(2)
+
+    def test_Update_empty_data(self):
         # steps Login
         browser = self.browser
         browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
@@ -36,31 +93,30 @@ class TestAddJobTitle(unittest.TestCase):
         browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div[7]/div/div/div[1]/div[2]/div/div/button[2]").click()
         time.sleep(3)
 
+    def test_Update_data(self):
+        # steps Login
+        browser = self.browser
+        browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        time.sleep(1)
+        browser.find_element(By.NAME, "username").send_keys("Admin")
+        time.sleep(1)
+        browser.find_element(By.NAME, "password").send_keys("admin123")
+        time.sleep(1)
+        browser.find_element(By.CLASS_NAME, "oxd-button").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH, "//span[normalize-space()='Admin']").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH, "//span[normalize-space()='Job']").click()
+        time.sleep(2)
+        browser.find_element(By.LINK_TEXT, 'Job Titles').click()
+        time.sleep(2)
 
-    # def test_Update_data(self):
-    #     # steps Login
-    #     browser = self.browser
-    #     browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    #     time.sleep(1)
-    #     browser.find_element(By.NAME, "username").send_keys("Admin")
-    #     time.sleep(1)
-    #     browser.find_element(By.NAME, "password").send_keys("admin123")
-    #     time.sleep(1)
-    #     browser.find_element(By.CLASS_NAME, "oxd-button").click()
-    #     time.sleep(2)
-    #     browser.find_element(By.XPATH, "//span[normalize-space()='Admin']").click()
-    #     time.sleep(2)
-    #     browser.find_element(By.XPATH, "//span[normalize-space()='Job']").click()
-    #     time.sleep(2)
-    #     browser.find_element(By.LINK_TEXT, 'Job Titles').click()
-    #     time.sleep(2)
+        # validasi
+        current_url = browser.current_url
+        self.assertEqual(current_url, "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList")
 
-    #     # validasi
-    #     current_url = browser.current_url
-    #     self.assertEqual(current_url, "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList")
-
-    #     browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div[7]/div/div/div[1]/div[2]/div/div/button[2]").click()
-    #     time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div[7]/div/div/div[1]/div[2]/div/div/button[2]").click()
+        time.sleep(3)
         
     
     def test_add_empty_data(self):
