34316daa0d8806c6c87a66b1dd8723e1b6bf927b
Mochammad Itmamul Wafa <itmamulwafa11@yahoo.com>
2023-02-26 09:55:38 +0700

    add job title

--- Test_Suite_Admin_Job_JobTilte/Add_job_title.py
+++ Test_Suite_Admin_Job_JobTilte/Add_job_title.py
@@ -10,23 +10,50 @@ class TestAddJobTitle(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
 
-    def test_a_add_success_job_title(self):
+
+    # def test_Update_data(self):
+    #     # steps Login
+    #     browser = self.browser
+    #     browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    #     time.sleep(1)
+    #     browser.find_element(By.NAME, "username").send_keys("Admin")
+    #     time.sleep(1)
+    #     browser.find_element(By.NAME, "password").send_keys("admin123")
+    #     time.sleep(1)
+    #     browser.find_element(By.CLASS_NAME, "oxd-button").click()
+    #     time.sleep(2)
+    #     browser.find_element(By.XPATH, "//span[normalize-space()='Admin']").click()
+    #     time.sleep(2)
+    #     browser.find_element(By.XPATH, "//span[normalize-space()='Job']").click()
+    #     time.sleep(2)
+    #     browser.find_element(By.LINK_TEXT, 'Job Titles').click()
+    #     time.sleep(2)
+
+    #     # validasi
+    #     current_url = browser.current_url
+    #     self.assertEqual(current_url, "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList")
+
+    #     browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div[7]/div/div/div[1]/div[2]/div/div/button[2]").click()
+    #     time.sleep(3)
+        
+    
+    def test_add_empty_data(self):
         # steps Login
         browser = self.browser
         browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        time.sleep(3)
+        time.sleep(1)
         browser.find_element(By.NAME, "username").send_keys("Admin")
-        time.sleep(3)
+        time.sleep(1)
         browser.find_element(By.NAME, "password").send_keys("admin123")
-        time.sleep(3)
+        time.sleep(1)
         browser.find_element(By.CLASS_NAME, "oxd-button").click()
-        time.sleep(5)
+        time.sleep(2)
         browser.find_element(By.XPATH, "//span[normalize-space()='Admin']").click()
-        time.sleep(3)
+        time.sleep(2)
         browser.find_element(By.XPATH, "//span[normalize-space()='Job']").click()
-        time.sleep(3)
+        time.sleep(2)
         browser.find_element(By.LINK_TEXT, 'Job Titles').click()
-        time.sleep(3)
+        time.sleep(2)
 
         # validasi
         current_url = browser.current_url
@@ -34,34 +61,115 @@ class TestAddJobTitle(unittest.TestCase):
 
         browser.find_element(By.XPATH, "//Button[normalize-space()='Add']").click()
         time.sleep(3)
-        browser.find_element(By.XPATH, "//input[normalize-space()='data-v-844e87dc']").send_keys("Desain Grafis")
+        browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[5]/button[2]").click()
         time.sleep(3)
 
-
-    # def test_a_delete_job_title(self):
+    def test_add_same_just_job_title(self):
         # steps Login
         browser = self.browser
         browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        time.sleep(3)
+        time.sleep(1)
         browser.find_element(By.NAME, "username").send_keys("Admin")
-        time.sleep(3)
+        time.sleep(1)
         browser.find_element(By.NAME, "password").send_keys("admin123")
-        time.sleep(3)
+        time.sleep(1)
         browser.find_element(By.CLASS_NAME, "oxd-button").click()
-        time.sleep(5)
+        time.sleep(2)
         browser.find_element(By.XPATH, "//span[normalize-space()='Admin']").click()
-        time.sleep(3)
+        time.sleep(2)
         browser.find_element(By.XPATH, "//span[normalize-space()='Job']").click()
-        time.sleep(3)
+        time.sleep(2)
         browser.find_element(By.LINK_TEXT, 'Job Titles').click()
-        time.sleep(3)
+        time.sleep(2)
 
         # validasi
         current_url = browser.current_url
         self.assertEqual(current_url, "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList")
 
-        delete = browser.find_element(By.XPATH, "//button").click().text
-        self.assertEqual("QA", delete)
+        browser.find_element(By.XPATH, "//Button[normalize-space()='Add']").click()
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input").send_keys("Manager Trainee")
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[5]/button[2]").click()
+        time.sleep(3)
+
+        # validation
+        response_data = browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[1]/div/span").text
+        self.assertIn('Already exists', response_data) 
+
+
+    def test_add_just_job_title(self):
+        # steps Login
+        browser = self.browser
+        browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        time.sleep(1)
+        browser.find_element(By.NAME, "username").send_keys("Admin")
+        time.sleep(1)
+        browser.find_element(By.NAME, "password").send_keys("admin123")
+        time.sleep(1)
+        browser.find_element(By.CLASS_NAME, "oxd-button").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH, "//span[normalize-space()='Admin']").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH, "//span[normalize-space()='Job']").click()
+        time.sleep(2)
+        browser.find_element(By.LINK_TEXT, 'Job Titles').click()
+        time.sleep(2)
+
+        # validasi
+        current_url = browser.current_url
+        self.assertEqual(current_url, "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList")
+
+        browser.find_element(By.XPATH, "//Button[normalize-space()='Add']").click()
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input").send_keys("Manager Trainee")
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[5]/button[2]").click()
+        time.sleep(3)
+
+    def test_a_add_success_job_title(self):
+        # steps Login
+        browser = self.browser
+        browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        time.sleep(1)
+        browser.find_element(By.NAME, "username").send_keys("Admin")
+        time.sleep(1)
+        browser.find_element(By.NAME, "password").send_keys("admin123")
+        time.sleep(1)
+        browser.find_element(By.CLASS_NAME, "oxd-button").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH, "//span[normalize-space()='Admin']").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH, "//span[normalize-space()='Job']").click()
+        time.sleep(2)
+        browser.find_element(By.LINK_TEXT, 'Job Titles').click()
+        time.sleep(2)
+
+        # validasi
+        current_url = browser.current_url
+        self.assertEqual(current_url, "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList")
+
+        browser.find_element(By.XPATH, "//Button[normalize-space()='Add']").click()
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input").send_keys("Desain Grafis")
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[2]/textarea").send_keys("Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
+        time.sleep(3)
+        # browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[3]/div/div[2]/div/i").click().send_keys("Test_Suite_Admin_Job_JobTilte\Final Project Batch 42.docx")
+        # time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[4]/div/div[2]/textarea").send_keys("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ")
+        time.sleep(3)
+        browser.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div/form/div[5]/button[2]").click()
+        time.sleep(3)
+
+
+    
+
+    
+
+
+
+
 
     def tearDown(self):
         self.browser.close()
